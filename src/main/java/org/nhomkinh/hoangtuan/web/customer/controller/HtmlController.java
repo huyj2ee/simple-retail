41576594047a29b4039cfef27f96d48e0674fb33
huyj2ee <huyj2ee@outlook.com>
2019-02-09 03:17:19 -0800

    [App] implement a small set of operation on: Price -- Product -- ManufactureCountry

--- src/main/java/org/nhomkinh/hoangtuan/web/customer/controller/HtmlController.java
+++ src/main/java/org/nhomkinh/hoangtuan/web/customer/controller/HtmlController.java
@@ -7,11 +7,19 @@ import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.nhomkinh.hoangtuan.web.customer.model.Product;
+import org.nhomkinh.hoangtuan.web.customer.model.Price;
+import org.nhomkinh.hoangtuan.web.customer.model.ManufactureCountry;
 import org.nhomkinh.hoangtuan.web.customer.repository.ProductRepository;
+import org.nhomkinh.hoangtuan.web.customer.repository.ManufactureCountryRepository;
+import java.util.Calendar;
+import java.sql.Date;
 
 
 @Controller
 public class HtmlController {
+  @Autowired
+  ManufactureCountryRepository originRepository;
+
   @Autowired
   private ProductRepository rep;
 
@@ -20,26 +28,95 @@ public class HtmlController {
     String msg = "";
     Iterable<Product> products = rep.findAll();
     for (Product product : products) {
-      msg += product.getCode();
+      msg += product.getCode() + ":";
+      msg += "\n\tOrigin -> " + product.getOrigin().getCode();
+      for (Price price : product.getPrices()) {
+          msg += "\n\t" + price.getDate().getTime();
+      }
+      msg += "\n";
     }
     model.addAttribute("appName", msg);
     return "index";
   }
 
-  @GetMapping("/populate")
-  public String populateDataPage(Model model) {
+  private String populateManufactureCountry() {
+    String[] codes = {
+      "vn",
+      "tw",
+      "cn"
+    };
     String msg = "";
+    // In case there is no ManufactureCountry, populate it
     try {
-      int i;
-      for (i = 0; i < 10; i++) {
-          Product p = new Product();
-          p.setCode("CD00" + i);
-          rep.save(p);
+      Iterable<ManufactureCountry> iter = originRepository.findAll();
+      int n = 0;
+      for (ManufactureCountry o : iter) {
+          n++;
       }
-      msg = "Succ";
+      if (n == 0) {
+        msg += "No ManufactureCountry found, popupate some data...\n";
+        for(int i = 0; i < codes.length; i++) {
+            ManufactureCountry mc = new ManufactureCountry();
+            mc.setCode(codes[i]);
+            originRepository.save(mc);
+        }
+      }
     }
     catch(DataAccessException daoException) {
-      msg = "DataAccessException";
+      msg += "DataAccessException_";
+    }
+    return msg;
+  }
+
+  @GetMapping("/populate")
+  public String populateDataPage(Model model) {
+    int day = 1;
+    Calendar cal = Calendar.getInstance();
+    cal.set(2019, 2, day++);
+    String msg = populateManufactureCountry();
+
+    String[] ccodes = {
+      "vn",
+      "vn",
+      "cn",
+      "cn",
+      "cn",
+      "tw",
+      "vn",
+      "tw",
+      "tw",
+      "tw",
+      "tw",
+      "cn"
+    };
+
+
+    try {
+      int i;
+      int j;
+      int n = 0;
+      for (i = 0; i < 10; i++) {
+        Product p = new Product();
+        p.setCode("CD00" + i);
+        ManufactureCountry mc = originRepository.findById(ccodes[i]).orElse(null);
+        if (i % 2 == 0) {
+          n++;
+        }
+        if (mc != null) {
+          p.setOrigin(mc);
+        }
+        for (j = 0; j < 2; j++) { 
+            Price v = new Price();
+            v.setDate(new Date(cal.getTime().getTime()));
+            cal.set(2019, 2, day++);
+            v.setProduct(p);
+        }
+        rep.save(p);
+      }
+      msg += "Succ";
+    }
+    catch(DataAccessException daoException) {
+      msg += "DataAccessException";
     }
     model.addAttribute("appName", msg);
     return "index";
---
+++ src/main/java/org/nhomkinh/hoangtuan/web/customer/repository/ManufactureCountryRepository.java
@@ -0,0 +1,10 @@
+package org.nhomkinh.hoangtuan.web.customer.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.nhomkinh.hoangtuan.web.customer.model.ManufactureCountry;
+
+
+public interface ManufactureCountryRepository extends CrudRepository<ManufactureCountry, String> {
+}
+
