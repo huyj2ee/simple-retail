4b8deff2bc94ca56d0a7afbce5439e3a9d3cd03f
huyj2ee <huyj2ee@outlook.com>
2019-02-20 00:35:31 -0800

    [App] remove @GeneratedValue in DimensionUnit and ProductUnit

--- src/main/java/org/nhomkinh/hoangtuan/web/customer/model/DimensionUnit.java
+++ src/main/java/org/nhomkinh/hoangtuan/web/customer/model/DimensionUnit.java
@@ -3,14 +3,10 @@ package org.nhomkinh.hoangtuan.web.customer.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-
 
 @Entity
 public class DimensionUnit implements Unit {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
 
   private String name;
--- src/main/java/org/nhomkinh/hoangtuan/web/customer/model/ProductUnit.java
+++ src/main/java/org/nhomkinh/hoangtuan/web/customer/model/ProductUnit.java
@@ -5,15 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class ProductUnit implements Unit {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
 
   private String name;
